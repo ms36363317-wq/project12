7b493761310ad0709c5f45001f2e5e305f45f5cc
ms36363317-wq <ms36363317@gmail.com>
2026-04-04 03:16:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from PIL import Image
 st.set_page_config(
     page_title="Eye Disease AI Diagnosis",
     page_icon="👁️",
-    layout="black",
+    layout="wide",
 )
  
 # ─── Custom CSS ─────────────────────────────────────────────────────────────
